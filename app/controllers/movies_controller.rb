0790cd8cf12601fb00e1bdebe0a87e05cf706189
quangvinh0513 <vinhnq@zigexn.vn>
2016-10-07 06:28:22 +0700

    fix heorku

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   def search
     if params[:search].present?
