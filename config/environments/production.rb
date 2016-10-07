33a389fb4f493a3dc19f561331042c9b5b2da428
quangvinh0513 <vinhnq@zigexn.vn>
2016-10-07 07:14:57 +0700

    jdksjkds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'pacific-castle-23068.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
-    :port           => "25",
+    :port           => "587",
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
